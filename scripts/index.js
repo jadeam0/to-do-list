5d1030f3ab30df851783a1f7d19ded16cc3d7d03
jadeam0 <jdpaulse27@gmail.com>
2023-06-02 16:16:07 +0200

    non-complete

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+let taskList = [];
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write soomething");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span =document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    savData();
+}
+
+listContainer.addEventListener('click', function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle('checked');
+        savData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        savData();
+    }
+}, false);
+
+function savData(){
+    localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function shwTask(){
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+shwTask();
